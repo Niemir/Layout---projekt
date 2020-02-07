700689a8e4b09b4e996e4eb8fab2acd677b80ea8
Marcin <niemir69@gmail.com>
2020-02-07 16:30:13 +0100

    add new photos to projects, add smooth navigation and change background handler

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-const backBurger = document.querySelector('.nav__item--back a');
+const backBurger = document.querySelector('.nav__item--back');
 const burger = document.querySelector('.burger');
 const sideMenu = document.querySelector('.nav__burger-menu');
 const page = document.querySelector('.page');
 const projects = document.querySelectorAll('.project--additional');
 const showMore = document.querySelector('.show-more');
+const arrows = document.querySelectorAll('.header__arrow');
+const header = document.querySelector('.header');
 
+//navigation
+const arrowUp = document.querySelector('.map__up');
+const servicesBtn = document.querySelector('.nav__services');
+const realizationsBtn = document.querySelector('.nav__realizations');
+const contactBtn = document.querySelector('.nav__contact');
 
 burgerHandler = () => {
   sideMenu.classList.toggle('nav__burger-menu--active');
@@ -33,10 +40,47 @@ projects.forEach(project => {
 
   })
 })
+
 window.addEventListener('resize', changeView)
 burger.addEventListener('click', burgerHandler);
 backBurger.addEventListener('click', burgerHandler);
 
+//bakcground change 
+arrows.forEach(arrow => {
+  arrow.addEventListener('click', () => {
+    const style = window.getComputedStyle(header);
+    const bgImage = style.getPropertyValue('background-image');
+    let index = bgImage.indexOf('/assets/');
+    let oldUrl = bgImage.slice(index);
+    if (oldUrl == '/assets/header-background.png")') {
+      header.style.backgroundImage = "url('../../assets/header-background2.jpg')";
+      return
+    }
+    let newIndex = window.getComputedStyle(header).backgroundImage.indexOf('/assets/');
+    let newUrl = window.getComputedStyle(header).backgroundImage.slice(newIndex)
+    if (newUrl == '/assets/header-background2.jpg")') {
+      header.style.backgroundImage = "url('../../assets/header-background.png')"
+    }
+
+  })
+}
+)
 
 
+//navigation
+arrowUp.addEventListener('click', () => {
+  document.querySelector('.nav').scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+})
+servicesBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log('dziala');
+  document.querySelector('.actions').scrollIntoView({ behavior: "smooth", block: "start" })
+})
+realizationsBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  document.querySelector('.projects').scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })
+})
+arrowUp.addEventListener('click', () => {
+  document.querySelector('.nav').scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+})
 
