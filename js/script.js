66bfa16a3ffd0e4bed20eecfe77090062ce8ab7e
Marcin <niemir69@gmail.com>
2020-02-07 12:58:55 +0100

    add media queries for sidepage <768px, add showmore btn handler

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const backBurger = document.querySelector('.nav__item--back a');
 const burger = document.querySelector('.burger');
 const sideMenu = document.querySelector('.nav__burger-menu');
 const page = document.querySelector('.page');
+const projects = document.querySelectorAll('.project--additional');
+const showMore = document.querySelector('.show-more')
 
 burgerHandler = () => {
   sideMenu.classList.toggle('nav__burger-menu--active');
@@ -19,9 +21,17 @@ changeView = () => {
     sideMenu.classList.remove('nav__burger-menu--active')
   }
 }
+
+projects.forEach(project => {
+  showMore.addEventListener('click', () => {
+    project.classList.remove('project--additional');
+    showMore.style.display = 'none'
+  })
+})
 window.addEventListener('resize', changeView)
 burger.addEventListener('click', burgerHandler);
 backBurger.addEventListener('click', burgerHandler);
+
 
 
 
